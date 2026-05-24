c821b745d8423065bbdb1c809a1e851c71d12d74
Lmo77 <luke@nunnas.care>
2026-05-23 22:07:17 -0600

    fix redis connection

--- src/app/api/activities/route.js
+++ src/app/api/activities/route.js
@@ -1,10 +1,7 @@
 import { NextResponse } from "next/server";
 import { Redis } from "@upstash/redis";
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
-});
+const redis = Redis.fromEnv();
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
