08b3487c8fb757fbf0493db1270a592b9bce8dc2
Lmo77 <luke@nunnas.care>
2026-05-25 16:39:46 -0600

    fix merge conflict

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = { output: 'standalone' };
-<<<<<<< HEAD
 module.exports = nextConfig;
-=======
-module.exports = nextConfig;
->>>>>>> feb3d078c7a9c8c6a9b1f73690e6434f3be17689
